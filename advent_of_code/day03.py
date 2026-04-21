86dcdaebca1916cfd03c4af97022e62dd6794091
Mark <mark@dymek.me>
2026-04-21 18:03:29 -0400

    Finished Day 04 aoc. Added script to automate puzzle creation files.

--- advent_of_code/day03.py
+++ advent_of_code/day03.py
@@ -26,8 +26,15 @@ def part_two(data):
     #:param data: List[str] of puzzle input lines
     #:return: Answer for Part Two
     """
-    # TODO: implement Part Two logic
-    return None
+    total = 0
+    priorities = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for i in range(0, len(data), 3):
+        group1 = data[i]
+        group2 = data[i+1]
+        group3 = data[i+2]
+        common_item = (set(group1) & set(group2) & set(group3)).pop()
+        total += priorities.index(common_item) + 1
+    return total
 
 
 def main():
